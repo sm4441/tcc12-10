4f9bcc01f245fb14064b9f33bfe4835460731a83
sm4441 <samuelribeirodossantossilva@gmail.com>
2025-11-13 15:55:59 -0300

    atualizações feitas por conta do banco de dados

--- src/DAO/vaga/addVaga.js
+++ src/DAO/vaga/addVaga.js
@@ -3,8 +3,8 @@ const { conexao } = require('../conexao.js');
 async function inserirVaga(nome, id_categoria, preco, id_empresa, is_pcd = false) {
     const sql = `
         INSERT INTO tbl_vaga 
-        (nome, id_categoria, preco, id_empresa, is_pcd)
-        VALUES (?, ?, ?, ?, ?)
+        (id_categoria, valor, id_empresa, is_pcd)
+        VALUES (?, ?, ?, ?)
     `;
 
     const conn = await conexao();
