7591f487640384d443bd3fa0904315c5323981c3
sm4441 <samuelribeirodossantossilva@gmail.com>
2025-11-14 17:36:04 -0300

    alteração em buscar vagas

--- src/DAO/vaga/buscarVaga.js
+++ src/DAO/vaga/buscarVaga.js
@@ -1,18 +1,22 @@
-const {conexao} = require('../conexao.js')
+const { conexao } = require('../conexao.js');
 
+async function listarVagasComDetalhes() {
+  const sql = `
+    SELECT 
+        v.id_vaga,
+        v.salario,
+        v.is_pcd,
+        v.descricao,
+        a.nome AS area_de_trabalho,
+        e.nome_fantasia AS nome_empresa
+    FROM tbl_vaga AS v
+    INNER JOIN tbl_areas_de_trabalho AS a
+        ON v.id_categoria = a.id
+    INNER JOIN tbl_empresa AS e
+        ON v.id_empresa = e.id;
+  `;
 
-async function buscarVaga(){
-    const sql = `SELECT * FROM tbl_vaga ;`
-    
-    const conn = await conexao()
-    try {
-        // Executar a consulta
-        const [rows, fields] = await conn.query(sql);
-        await conn.end()
-        return rows
-      } catch (err) {
-        return err.message
-      }
+  const conn = await conexao();
+  const [rows] = await conn.query(sql);
+  return rows;
 }
-
-module.exports = {buscarVaga}
