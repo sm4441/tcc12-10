8b544bb9c2855e1e5cd9fd828a4b81aa8e7eda1a
davividahd <daviricardosantos2007@gmail.com>
2025-11-22 21:15:06 -0300

    adaptado para o pool global

--- src/DAO/Empresa/buscarEmpresa.js
+++ src/DAO/Empresa/buscarEmpresa.js
@@ -1,5 +1,24 @@
 
 //funcionando
+const { pool } = require('../conexao');
+
+
+async function buscarEmpresa(){
+    const sql = `SELECT * FROM tbl_empresa;`
+    
+    const conn = await conexao()
+    try {
+        // Executar a consulta
+        const [rows, fields] = await conn.query(sql);
+        await conn.end()
+        return rows
+      } catch (err) {
+        return err.message
+      }
+}
+
+module.exports = {buscarEmpresa}
+//funcionando
 const {conexao} = require('../conexao.js')
 
 
