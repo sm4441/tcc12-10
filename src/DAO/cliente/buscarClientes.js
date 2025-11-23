6e570ebfc14a2b925a7c6e26353cd34eb8e70402
davividahd <daviricardosantos2007@gmail.com>
2025-11-23 14:49:20 -0300

    Update buscarClientes.js
    arrumei pro pool funcionar

--- src/DAO/cliente/buscarClientes.js
+++ src/DAO/cliente/buscarClientes.js
@@ -1,34 +1,31 @@
-
-//funcionando
 const { pool } = require('../conexao');
 
+async function buscarClientes() {
+  try {
+    const sql = `
+      SELECT 
+        c.cpf,
+        c.nome_completo,
+        c.telefone,
+        c.email,
+        e.logradouro,
+        e.numero,
+        e.bairro,
+        e.cidade,
+        e.cep
+      FROM tbl_candidato AS c
+      INNER JOIN tbl_endereco_do_candidato AS e
+        ON c.id_endereco = e.id;
+    `;
 
-async function buscarClientes(){
-   const sql = `
-    SELECT 
-      c.cpf,
-      c.nome_completo,
-      c.telefone,
-      c.email,
-      e.logradouro,
-      e.numero,
-      e.bairro,
-      e.cidade,
-      e.cep
-    FROM tbl_candidato AS c
-    INNER JOIN tbl_endereco_do_candidato AS e
-      ON c.id_endereco = e.id;
-  `;
-    
-    const conn = await conexao()
-    try {
-        // Executar a consulta
-        const [rows, fields] = await conn.query(sql);
-        await conn.end()
-        return rows
-      } catch (err) {
-        return err.message
-      }
+    // Executar a consulta usando pool
+    const [rows] = await pool.query(sql);
+
+    return rows;
+
+  } catch (err) {
+    return { erro: err.message };
+  }
 }
 
-module.exports = {buscarClientes}
+module.exports = { buscarClientes };
