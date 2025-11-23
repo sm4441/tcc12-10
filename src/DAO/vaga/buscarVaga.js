d6257c3220c89b121916f75ea6615b69c3483dfa
davividahd <daviricardosantos2007@gmail.com>
2025-11-23 14:54:23 -0300

    Update buscarVaga.js
    arrumei pro pool funcionar

--- src/DAO/vaga/buscarVaga.js
+++ src/DAO/vaga/buscarVaga.js
@@ -1,26 +1,30 @@
-const { conexao } = require('../conexao.js');
+const { pool } = require('../conexao');
 
 async function listarVagasComDetalhes() {
-  const sql = `
-    SELECT 
-        v.id_vaga,
-        v.salario,
-        v.is_pcd,
-        v.descricao,
-        a.nome AS area_de_trabalho,
-        e.nome AS nome_empresa,
-        e.cidade AS cidade_empresa,
-        e.estado AS estado_empresa
-    FROM tbl_vaga AS v
-    INNER JOIN tbl_areas_de_trabalho AS a
-        ON v.id_categoria = a.id
-    INNER JOIN tbl_empresa AS e
-        ON v.id_empresa = e.id;
-  `;
+    const sql = `
+        SELECT 
+            v.id_vaga,
+            v.salario,
+            v.is_pcd,
+            v.descricao,
+            a.nome AS area_de_trabalho,
+            e.nome AS nome_empresa,
+            e.cidade AS cidade_empresa,
+            e.estado AS estado_empresa
+        FROM tbl_vaga AS v
+        INNER JOIN tbl_areas_de_trabalho AS a
+            ON v.id_categoria = a.id
+        INNER JOIN tbl_empresa AS e
+            ON v.id_empresa = e.id;
+    `;
 
-  const conn = await conexao();
-  const [rows] = await conn.query(sql);
-  return rows;
+    try {
+        const [rows] = await pool.query(sql);
+        return rows;
+    } catch (err) {
+        console.error("Erro ao listar vagas com detalhes:", err);
+        return { sucesso: false, erro: err.message };
+    }
 }
 
 module.exports = { listarVagasComDetalhes };
